8e51c9ea77a9c7c6b83294ac98dca6e37a31670e
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-05-24 20:53:02 -0600

    feat: Add page input param binding

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
 import { provideZard } from '@/shared/core/provider/providezard';
 import { registerLocaleData } from '@angular/common';
 import localeEsMx from '@angular/common/locales/es-MX';
-import { routes } from './app.routes';;
+import { routes } from './app.routes';
 
 registerLocaleData(localeEsMx);
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideZard()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' }),
+      withComponentInputBinding(),
+    ),
+    provideZard(),
+  ],
 };
