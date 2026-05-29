186ea49d7368e76ecc8f5b7fe5fefdcd5164e8a0
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-05-29 08:36:21 -0600

    Revert "feat: Install zardui dialog"
    This reverts commit bc8bd7110b2569c1560c454244f997963dfebb19.

--- src/app/shared/components/dialog/index.ts
+++
@@ -1,6 +0,0 @@
-export { ZardDialogComponent, ZardDialogOptions } from './dialog.component';
-export { type OnClickCallback as DialogOnClickCallback } from './dialog.component';
-export * from './dialog.service';
-export * from './dialog-ref';
-export * from './dialog.variants';
-export * from './dialog.imports';
