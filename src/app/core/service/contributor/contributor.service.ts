14db157aae355197de13939f6ec714d527627511
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-05-31 18:47:35 -0600

    feat: Add contributor logout button

--- src/app/core/service/contributor/contributor.service.ts
+++ src/app/core/service/contributor/contributor.service.ts
@@ -11,17 +11,11 @@ export class ContributorService {
   private readonly registerApi = API.CONTRIBUTORS.REGISTER;
   private readonly profileApi = API.CONTRIBUTORS.PROFILE;
   private readonly client = inject(HttpClient);
-  sessionId = signal(localStorage.getItem('sessionId'));
-  // sessionId = computed(() => {
-  //   const sessionId = this.nullableSessionId();
-  //   if (!sessionId) throw new Error('No session ID found. User might not be logged in.');
-  //   return sessionId;
-  // });
 
+  sessionId = signal(localStorage.getItem('sessionId'));
   access(data: { fullName: string }) {
     return this.client.post<{ message: string; sessionId: string }>(this.registerApi, data).pipe(
       tap(({ sessionId }) => {
-        console.log('Received session ID:', sessionId); // Debug log
         localStorage.setItem('sessionId', sessionId);
         this.sessionId.set(sessionId);
       }),
@@ -36,5 +30,10 @@ export class ContributorService {
 
   getProfile(sessionId: string = this.sessionId() || '') {
     return this.client.get<Contributor>(this.profileApi(sessionId));
+  }
+
+  logout() {
+    localStorage.removeItem('sessionId');
+    this.sessionId.set(null);
   }
 }
--- src/app/ui/organisms/top-app-bar/top-app-bar.ts
+++ src/app/ui/organisms/top-app-bar/top-app-bar.ts
@@ -1,8 +1,11 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ContributorService } from '@/core/service/contributor/contributor.service';
+import { ZardButtonComponent } from '@/shared/components/button';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tm-top-app-bar',
-  imports: [],
+  imports: [ZardButtonComponent],
   templateUrl: './top-app-bar.html',
   styleUrl: './top-app-bar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -10,4 +13,14 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 export class TopAppBar {
   readonly title = input('Techmati');
   readonly subtitle = input('');
+
+  private readonly contributorService = inject(ContributorService);
+  private readonly router = inject(Router);
+
+  logout() {
+    console.log('Logging out...'); // Debug log
+    this.contributorService.logout();
+    this.router.navigate(['/']);
+    console.log('Logged out'); // Debug log
+  }
 }
