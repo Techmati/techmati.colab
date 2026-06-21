363266a8401b0397a50641ad14b5bd57623af3a9
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-06-20 19:31:03 -0600

    refactor: Add input group component to phrase set toolbar

--- src/app/ui/pages/admin-phrase-sets/ui/organisms/admin-phrase-sets-toolbar/admin-phrase-sets-toolbar.ts
+++ src/app/ui/pages/admin-phrase-sets/ui/organisms/admin-phrase-sets-toolbar/admin-phrase-sets-toolbar.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { ZardButtonComponent } from '@/shared/components/button';
 import { ZardInputDirective } from '@/shared/components/input';
+import { ZardInputGroupComponent } from '@/shared/components/input-group';
 
 @Component({
   selector: 'tm-admin-phrase-sets-toolbar',
-  imports: [ZardButtonComponent, ZardInputDirective],
+  imports: [ZardButtonComponent, ZardInputDirective, ZardInputGroupComponent],
   templateUrl: './admin-phrase-sets-toolbar.html',
   styleUrl: './admin-phrase-sets-toolbar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminPhraseSetsToolbar {}
+export class AdminPhraseSetsToolbar { }
