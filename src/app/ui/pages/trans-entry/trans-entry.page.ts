51ac56c5d3c5ffd487d054c8ee5fea36274275c6
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-07-15 23:17:00 -0600

    feat: Add retry phrase set option

--- src/app/ui/pages/trans-entry/trans-entry.page.ts
+++ src/app/ui/pages/trans-entry/trans-entry.page.ts
@@ -1,15 +1,17 @@
 import { ContributorContextService } from '@/core/service/contributor-context/contributor-context.service';
 import { TranslationService } from '@/core/service/translation/translation.service';
 import { ZardDividerComponent } from '@/shared/components/divider';
+import { DialectSelectionDialog } from '@/ui/organisms/dialect-selection-dialog/dialect-selection-dialog';
 import { Location } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, effect, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { WavesAudioPlayer } from '../../molecules/waves-audio-player/waves-audio-player';
 import { TransEntrySkeleton } from './ui/organisms/trans-entry-skeleton/trans-entry-skeleton';
 
 @Component({
   selector: 'tm-trans-entry-page',
-  imports: [WavesAudioPlayer, ZardDividerComponent, TransEntrySkeleton],
+  imports: [WavesAudioPlayer, ZardDividerComponent, TransEntrySkeleton, DialectSelectionDialog],
   templateUrl: './trans-entry.page.html',
   styleUrl: './trans-entry.page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,9 +20,10 @@ export class TransEntryPage {
   readonly translationId = input.required<string>();
   readonly title = input<string>('');
 
-  private readonly location = inject(Location);
   private readonly translationService = inject(TranslationService);
   private readonly contributorContext = inject(ContributorContextService);
+  private readonly location = inject(Location);
+  private readonly router = inject(Router);
 
   protected readonly entryRes = injectQuery(() => {
     const contributor = this.contributorContext.active()!;
@@ -37,6 +40,9 @@ export class TransEntryPage {
     effect(() => console.log(this.entryRes.data()));
   }
 
+  protected onTranslationCreated(translationId: string): void {
+    this.router.navigate(['/translate', translationId]);
+  }
   protected goBack(): void {
     this.location.back();
   }
